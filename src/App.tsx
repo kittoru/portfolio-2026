0c6b5a321e59dda966fa20530962af1fb5aa7844
Elizaveta Gnetneva <mama190774@gmail.com>
2026-08-14 17:02:05 +0300

    feat: add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Footer } from '@components/Footer'
 import { Header } from '@components/Header'
 
 function App() {
   return (
-    <section className="max-w-300 mx-auto px-3 md:px-0">
+    <section className="flex flex-col max-w-300 min-h-screen min-h-dvh md:min-h-screen mx-auto px-3 md:px-0">
       <Header />
+      <main className="flex-1">main</main>
+      <Footer />
     </section>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+export const Footer = () => (
+  <footer className="text-center mb-5">
+    <span className="font-light text-[14px] tracking-[6%]">2026</span>
+  </footer>
+)
